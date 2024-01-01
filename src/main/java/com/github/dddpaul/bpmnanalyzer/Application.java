7d1436d01a6b77c6d2d3253fe7c42fe45dace8c1
Pavel Derendyaev <dddpaul@gmail.com>
2024-01-01 18:43:19 +0300

    Add process id

--- src/main/java/com/github/dddpaul/bpmnanalyzer/Application.java
+++ src/main/java/com/github/dddpaul/bpmnanalyzer/Application.java
@@ -2,7 +2,9 @@ package com.github.dddpaul.bpmnanalyzer;
 
 import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import io.camunda.zeebe.model.bpmn.instance.BaseElement;
 import io.camunda.zeebe.model.bpmn.instance.ServiceTask;
+import io.camunda.zeebe.model.bpmn.instance.Process;
 import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeTaskDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,12 +31,17 @@ public class Application {
     public void start() throws IOException {
         try (InputStream stream = Files.newInputStream(Path.of(processFileName))) {
             BpmnModelInstance instance = Bpmn.readModelFromStream(stream);
+            Collection<Process> processes = instance.getDocumentElement().getChildElementsByType(Process.class);
+            String processId = processes.stream()
+                    .findFirst()
+                    .map(BaseElement::getId)
+                    .orElseThrow();
             Collection<ServiceTask> tasks = instance.getModelElementsByType(ServiceTask.class);
             List<?> types = tasks.stream()
                     .map(task -> task.getSingleExtensionElement(ZeebeTaskDefinition.class))
                     .map(ZeebeTaskDefinition::getType)
                     .toList();
-            System.out.println("Task types = " + types);
+            System.out.printf("process: %s, tasks: %s\n", processId, types);
         }
     }
 

4f72f49d192fdf66ad54eb8a17aec7a7d4defedd
Pavel Derendyaev <dddpaul@gmail.com>
2024-01-01 17:54:02 +0300

    Remove spring boot dep

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: build
 
 build:
-	@./gradlew clean bootJar
+	@./gradlew clean shadowJar
 
 analyzer:
-	@java -jar build/libs/spring-bpmn-analyzer-0.0.1-SNAPSHOT.jar
+	@java -jar build/libs/spring-bpmn-analyzer-0.0.1-SNAPSHOT-all.jar
--- src/main/java/com/github/dddpaul/bpmnanalyzer/Application.java
+++ src/main/java/com/github/dddpaul/bpmnanalyzer/Application.java
@@ -2,32 +2,32 @@ package com.github.dddpaul.bpmnanalyzer;
 
 import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
-import io.camunda.zeebe.model.bpmn.instance.FlowElement;
 import io.camunda.zeebe.model.bpmn.instance.ServiceTask;
 import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeTaskDefinition;
-import org.camunda.bpm.model.xml.type.attribute.Attribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 
-@Component
-@ConditionalOnProperty(value = "app.analyzer.enabled", havingValue = "true")
-public class BpmnAnalyzer {
+public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(BpmnAnalyzer.class);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    @Value("${app.analyzer.process-file}")
-    private String processFileName;
+    @Option(names = {"-p", "--process"}, description = "BPMN process file name", required = true)
+    private String processFileName = "";
+
+    @Option(names = {"-h", "--help"}, description = "Display a help message", usageHelp = true)
+    private boolean help = false;
 
     public void start() throws IOException {
-        try (InputStream stream = getClass().getClassLoader().getResourceAsStream(processFileName)) {
+        try (InputStream stream = Files.newInputStream(Path.of(processFileName))) {
             BpmnModelInstance instance = Bpmn.readModelFromStream(stream);
             Collection<ServiceTask> tasks = instance.getModelElementsByType(ServiceTask.class);
             List<?> types = tasks.stream()
@@ -36,5 +36,14 @@ public class BpmnAnalyzer {
                     .toList();
             log.info("BPMN schema contains tasks: {}", types);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Application app = CommandLine.populateCommand(new Application(), args);
+        if (app.help) {
+            CommandLine.usage(app, System.err);
+            return;
+        }
+        app.start();
     }
 }
